de770598719dafc7f51f88f5ed25a84bb7cefdfa
iastac84 <istacey@outlook.com>
2024-02-16 18:08:39 +0000

    100daysofpython-thenext100

---
+++ test-in-string-nested-if.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TEAM_NAME="LUTON TOWN FC"
+SEARCH="Liverpool"
+TIER=3
+
+
+if [[ $(echo "$TEAM_NAME" | tr '[:upper:]' '[:lower:]') =~ $(echo "$SEARCH" | tr '[:upper:]' '[:lower:]') ]]; then
+    echo "We are Luton Town"
+else
+    echo "We searched for $SEARCH"
+    if [[ ${TIER} = 1 ]]; then
+        echo "Tier 1"
+    elif [[ ${TIER} = 2 ]]; then
+        echo "Tier 2"
+    elif [[ ${TIER} = 3 ]]; then
+        echo "Tier 3"
+    elif [[ ${TIER} = 4 ]]; then
+        echo "Tier 4"
+    fi
+fi
