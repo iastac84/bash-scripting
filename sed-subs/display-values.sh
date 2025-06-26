28c387d21654deb590a9836995301e59e088fdd9
iastac84 <istacey@outlook.com>
2025-06-26 16:10:03 +0100

    sed-subs

--- sed-subs/display-values.sh
+++ sed-subs/display-values.sh
@@ -2,12 +2,11 @@
 
 # Loop through each line in the CSV
 while IFS=',' read -r name age sex; do
-  cp template.txt "$name.txt"
-  sed -i \
+  sed \
     -e "s/NAME/$name/" \
     -e "s/AGE/$age/" \
     -e "s/SEX/$sex/" \
-    "$name.txt"
-  echo "Created $name.txt"
+    template.txt
+  echo "---"
 done < data.csv
 
